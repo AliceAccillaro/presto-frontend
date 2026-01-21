b7e8b358d0aefd6ee12dc9a031535ab036dcd747
AliceAccillaro <aaccillaro@gmail.com>
2026-01-21 01:13:14 +0100

    =aggiunte recensioni

--- main.js
+++ main.js
@@ -61,3 +61,68 @@ let observer = new IntersectionObserver((entries) => {
 observer.observe(firstnumber);
 observer.observe(secondnumber);
 observer.observe(thirdnumber);
+
+
+// recensioni
+let reviews = [
+    { user: `Jasmine`, description: `"Geniale"`, rank: 5 },
+    { user: `Aladdin`, description: `"Ottimo servizio"`, rank: 4 },
+    { user: `Sultan`, description: `"Consigliatissimo!"`, rank: 5 },
+    { user: `Abu`, description: `"Esperienza mediocre"`, rank: 2 },
+    { user: `Raja`, description: `"Roar"`, rank: 5 },
+    { user: `Jafar`, description: `"Sono disgustato"`, rank: 1 },
+]
+
+let swiperWrapper = document.querySelector('.swiper-wrapper');
+
+reviews.forEach((recensione) => {
+    let div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+        <div class="swiper-slide">
+            <div class="card-review">
+                <p class="lead text-center margin-custom fs-1">${recensione.description}</p>
+                <p class="h4 text-center">${recensione.user}</p>
+                <div class="d-flex justify-content-center star">
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+                </div>
+            </div>
+        </div>`;
+    swiperWrapper.appendChild(div);
+});
+
+let stars = document.querySelectorAll('.star');
+
+stars.forEach((star, index) => {
+    for (let i = 1; i <= reviews[index].rank; i++) {
+        let icon = document.createElement('i');
+        icon.classList.add('fa-solid', 'fa-star');
+        star.appendChild(icon);
+    }
+
+    let difference = 5 - reviews[index].rank;
+    for (let i = 1; i <= difference; i++) {
+        let icon = document.createElement('i');
+        icon.classList.add('fa-regular', 'fa-star');
+        star.appendChild(icon);
+    }
+
+});
+
+// swiper
+
+var swiper = new Swiper(".mySwiper", {
+    rewind: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+
+    // // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+});
